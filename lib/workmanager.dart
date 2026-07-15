63232c21d684c8a614818761b469f28cfb7e56b8
Cody Goldberg <cody@goldberg.fyi>
2026-07-15 12:50:04 -0400

    workmanager: replace fork with standalone package

---
+++ lib/workmanager.dart
@@ -0,0 +1,7 @@
+export 'src/workmanager_impl.dart'
+    show
+        BackgroundTaskHandler,
+        ForegroundServiceType,
+        Workmanager,
+        WorkmanagerForegroundOptions;
+export 'package:workmanager_platform_interface/workmanager_platform_interface.dart';
