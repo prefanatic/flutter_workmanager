cd12902db944765ff1f6a56b1ce5efb3ab6f8c09
Cody Goldberg <cody@goldberg.fyi>
2026-07-15 12:55:04 -0400

    Revert "workmanager: replace fork with standalone package"
    This reverts commit 63232c21d684c8a614818761b469f28cfb7e56b8.

--- lib/workmanager.dart
+++
@@ -1,7 +0,0 @@
-export 'src/workmanager_impl.dart'
-    show
-        BackgroundTaskHandler,
-        ForegroundServiceType,
-        Workmanager,
-        WorkmanagerForegroundOptions;
-export 'package:workmanager_platform_interface/workmanager_platform_interface.dart';
